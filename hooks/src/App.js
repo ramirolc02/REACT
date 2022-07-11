4148bb8bcf941376d6d48c7a6d3c07ef8e4ef5b5
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-07-11 16:31:17 +0200

    useState hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,17 +1,37 @@
 import { useState } from 'react' // import function
 
+// Destructuring, not passing props but just necessary objects.
+const Display = ({counter}) => <divs> {counter} </divs>
+
+const Button = ({onClick,text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0) // counter = 0 && setcounter is function that modifies state
 
-  setTimeout(
+  // variables containing reference to function
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter -1)
+  const setToZero = () => setCounter(0)
+
+  // Set timeout adds 1 to counter each second.
+  /* setTimeout(
     () => setCounter(counter + 1),
     1000
-  )
-
-  console.log('rendering...', counter)
-
+  ) */ 
   return (
-    <div>{counter}</div>
+    <div>
+        <h1>App</h1>
+        <Display counter = {counter}/>
+        <Button onClick={increaseByOne}
+          text='plus'
+        />
+         <Button onClick={setToZero}
+          text='zero'
+        />
+         <Button onClick={decreaseByOne}
+          text='minus'
+        />
+    </div>
   )
 }
 
