78ef0a1ff7da7450709d56d823ee594fb85d829c
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-08-10 10:48:35 +0200

    Functions that return functions

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -30,9 +30,47 @@ const App = () => {
     setRight(right + 1)
   }
 
+  // Function that returns a function.
+  const hello = (who) => () => {
+    console.log('Welcome', who)
+  }
+
+  const[value, setValue] = useState(10) 
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue) // prints new value
+    setValue(newValue)
+  }
+
+ /* "Normal" way to achieve the same 
+
+ const setToValue = (newValue) => {
+  console.log('value now', newValue)
+  setValue(newValue)
+}   
+
+return (
+    <div>
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+    </div>
+  )
+} */
+
   return (
     <div>
       <h1> Welcome </h1>
+      {value}
+     <button onClick={hello('user')}> button </button>
+     <button onClick={setToValue(1000)}> thousand </button>
+     <button onClick={setToValue(0)}>reset</button>
       {left}
       <Button handleClick={handleLeftClick} text='left' />
       <Button handleClick={handleRightClick} text='right' />
@@ -40,7 +78,7 @@ const App = () => {
       <History allClicks={allClicks}/> 
     </div>
   )
-  // join(separated by string passes as function parameter)
+  // join(separated by string passed as function parameter)
 }
 
 export default App
