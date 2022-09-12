35579afcbd5f1c8b5107c7a0668d5e342abafa90
Rama Lopez Cento <ramirolopezcento@icloud.com>
2022-09-12 18:34:13 +0200

    Post responses

--- node_backend/index.js
+++ node_backend/index.js
@@ -1,5 +1,8 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -20,17 +23,61 @@ let notes = [
       date: "2022-05-30T19:20:14.298Z",
       important: true
     }
-  ]
+  ] 
 
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
+  })
+  
+  // if undefined, evaluates to false
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
+    const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
+  
+  app.delete('/api/notes/:id',(request,response)=>{
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
     response.json(note)
   })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
